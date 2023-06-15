692be8295bbd0b2dfabbbf66152664c469e69b24
Ümmügülsüm Yücel <gulsum.yucel5534@gmail.com>
2023-06-16 00:23:43 +0300

    yeni tema eklendi

---
+++ lib/app/constants/assets.dart
@@ -0,0 +1,11 @@
+class Assets {
+  const Assets._();
+
+  final logo = 'assets/resimler/logo.png';
+  final reklam = 'assets/resimler/reklam.png';
+  final reklam1 = 'assets/resimler/reklam1.png';
+  final reklam2 = 'assets/resimler/reklam2.png';
+  final dalga = 'assets/resimler/dalga.png';
+  final otel = 'assets/resimler/otel.png';
+  final otelic = 'assets/resimler/oteliç.jpg';
+}
