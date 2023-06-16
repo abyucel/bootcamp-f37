54b43cdeb005e2c89f89009812cdbf29bbbc8fa2
Mustafa KARA <mustafa.kara2@std.hku.edu.tr>
2023-06-16 23:02:27 +0300

    Added: Roboto Font Group Fix: Static Color Bugs Created: EasyButton

--- lib/app/constants/theme.dart
+++ lib/app/constants/theme.dart
@@ -4,44 +4,44 @@ class AppColors {
   const AppColors._();
 
   //welcome page
-  static const Color anaRenk = Color(0x3D8BD1);
-  static const Color girisyapbutonu = Color(0x56AEFF);
-  static const Color misafirGirisFont = Color(0xCCCCCC);
+  static const Color anaRenk = Color(0xFF3D8BD1);
+  static const Color girisyapbutonu = Color(0xFF56AEFF);
+  static const Color misafirGirisFont = Color(0xFFCCCCCC);
 
   //kayıt ol
 
-  static const Color userLogo = Color(0x9094AA);
-  static const Color kayitOlFont = Color(0x98A8C6);
+  static const Color userLogo = Color(0xFF9094AA);
+  static const Color kayitOlFont = Color(0xFF98A8C6);
 
   //giriş
 
-  static const Color userLogosu = Color(0x4864E6);
-  static const Color sifreLogosu = Color(0x276DF0);
-  static const Color girisFont = Color(0x276DF0);
+  static const Color userLogosu = Color(0xFF4864E6);
+  static const Color sifreLogosu = Color(0xFF276DF0);
+  static const Color girisFont = Color(0xFF276DF0);
 
   //Homepage
 
-  static const Color yildizFont = Color(0xFFF500);
+  static const Color yildizFont = Color(0xFFFFF500);
 
   // Arama Sayfası
-  static const Color koyuGri = Color(0xF3F3F3);
-  static const Color agacLogo = Color(0x319F43);
-  static const Color aramaLogosu = Color(0x757575);
-  static const Color siyah = Color(0x000000);
-  static const Color otelAra = Color(0xA8A8A8);
+  static const Color koyuGri = Color(0xFFF3F3F3);
+  static const Color agacLogo = Color(0xFF319F43);
+  static const Color aramaLogosu = Color(0xFF757575);
+  static const Color siyah = Color(0xFF000000);
+  static const Color otelAra = Color(0xFFA8A8A8);
 
   // Filtreleme Sayfası
 
-  static const Color gri = Color(0x494949);
-  static const Color odaSayisiDerece = Color(0x8AC3F5);
-  static const Color koyuMavi = Color(0x3F81BC);
+  static const Color gri = Color(0xFF494949);
+  static const Color odaSayisiDerece = Color(0xFF8AC3F5);
+  static const Color koyuMavi = Color(0xFF3F81BC);
 
   // Otel Profili
 
-  static const Color toplamYildiz = Color(0xFBE201);
+  static const Color toplamYildiz = Color(0xFFFBE201);
 
   //Profil
 
-  static const Color cizgiRenk = Color(0x073864);
-  static const Color cikisYap = Color(0xFF0000);
+  static const Color cizgiRenk = Color(0xFF073864);
+  static const Color cikisYap = Color(0xFFFF0000);
 }
---
+++ lib/view/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../app/utilities/Button.dart';
+
+class homePage extends StatefulWidget {
+  homePage({Key? key}) : super(key: key);
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Colors.white, body: Container());
+  }
+}
