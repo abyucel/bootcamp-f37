9d114fcfb9ad55e175e669d4348ca5d6be00d258
Ümmügülsüm Yücel <gulsum.yucel5534@gmail.com>
2023-06-26 23:46:50 +0300

    profil sayfasi eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ekotel/girissayfasi.dart';
+import 'package:ekotel/view/profilesayfasi.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'firebase_options.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GirisSayfasi(),
+      home: ProfilSayfasi(),
     );
   }
 }
